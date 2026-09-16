a8246dc09ca68518647c70bc29e91fcf52956751
Marcos Drien <teacher.mrdv@gmail.com>
2026-09-16 11:55:47 +0800

    recursion HL

---
+++ HL/Recursion/FibonacciRecursive.java
@@ -0,0 +1,24 @@
+
+public class FibonacciRecursive
+{
+	static int c = 0;
+	
+	public static int fibo(int term)
+	{
+		c++;
+		if(term == 0)
+		{	return 0;
+		}
+		if(term == 1)
+		{	return 1;
+		}
+		return fibo(term-1) + fibo(term-2);
+	}
+	public static void main (String[] args)
+	{
+		System.out.println( fibo(8) );
+		System.out.println( c );
+		//System.out.println( term );
+	}
+}
+
