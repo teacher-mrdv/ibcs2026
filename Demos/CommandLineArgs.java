a3eeed979bdab7e924acefae9ec83f40b9b68152
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2026-09-23 14:49:07 +0800

    Create CommandLineArgs.java

---
+++ Demos/CommandLineArgs.java
@@ -0,0 +1,14 @@
+
+public class CommandLineArgs {
+	
+	public static void main (String[] args) {
+		if(args.length > 0) {
+			for(int i = 0; i < args.length; i++) {
+				System.out.println(i + " : " + args[i]);
+			}
+		} else {
+			System.out.println("No arguments passed.");
+		}
+	}
+}
+
